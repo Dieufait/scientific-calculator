dfc1f1350af7e0e30e669ac9a4ce2a09fed7e2cd
foulart <44730236+Dieufait@users.noreply.github.com>
2023-04-17 04:14:24 +0000

    Added change calc

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
          // your Javascript code gestion here
-         const display = document.getElementById('calc-display');
+         const display = document.getElementById('calc-displays');
          const buttons = document.getElementsByClassName('btn');
 
 
@@ -11,8 +11,16 @@ document.addEventListener("DOMContentLoaded", function(){
            const convertedValue = currentValue
              .replace("×", "*")
              .replace("÷", "/")
-             .replace("%", "*0.01");
-           const result = eval(convertedValue)
+             .replace("%", "*0.01")
+             .replace("sin", "Math.sin")
+             .replace("cos", "Math.cos")
+             .replace("π", "Math.PI")
+             .replace("ln", "Math.log")
+             .replace("log", "Math.log10")
+             .replace("e", "Math.E")
+             .replace("tan", "Math.tan")
+             .replace("√", "Math.sqrt");
+           const result = eval(convertedValue);
            currentValue = result.toString();
            display.value = currentValue;
          }
@@ -23,15 +31,23 @@ document.addEventListener("DOMContentLoaded", function(){
            button.addEventListener('click', function(){
               const value = button.innerText;
 
-             if(value=="AC"){
+          try {
+            if(value=="AC"){
                currentValue = "";
-               display.value = currentValue;
+               display.value =currentValue;
              }else if(value=="="){
                 evaluateResult();
              }else{
               currentValue += value;
               display.value = currentValue;
              }
+             
+          } catch (error) {
+             currentValue = "ERROR";
+             display.value = currentValue;
+          }
+          
+             
              
            })
 
